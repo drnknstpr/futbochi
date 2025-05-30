a511d9a3b542b6fd78c203afe097c115a334d1de
drnknstpr <137000834+drnknstpr@users.noreply.github.com>
2025-05-30 11:37:07 +0300

    Add files via upload

--- bot_main_futbotchi.py
+++ bot_main_futbotchi.py
@@ -1,15 +1,1 @@
-# bot_main_futbotchi.py
-from telegram.ext import Application, CommandHandler
-from storage import get_user, save_user
-from basic_handlers import start_handler, profile_handler
-
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-app = Application.builder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start_handler))
-app.add_handler(CommandHandler("profile", profile_handler))
-
-if __name__ == "__main__":
-    app.run_polling()
+# main bot entry point
